5389574945d7ad78c8fd282734194a16ce4da972
geortron <george.trontsios@gmail.com>
2020-01-04 13:32:40 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const request = require('request-promise').defaults({jar: true});
+const fs = require('fs');
+
+async function main() {
+	try{
+		const html = await request.post('https://accounts.craigslist.org/login', {
+		    form: {
+		      inputEmailHandle: "george.trontsios@gmail.com",
+		      inputPassword: "123!@#wersdf"
+		    },
+		    headers: {
+		       Referer: 'https://accounts.craigslist.org/login'
+		    },
+		    
+		    simple: false,
+		   // followAllRedirects: true
+        });
+
+        fs.writeFileSync('./login.html', html);
+        const billingHtml = await request.get('https://accounts.craigslist.org/login/home?show_tab=billing');
+        fs.writeFileSync('./log.html', billingHtml);
+
+    } catch (error) {
+      console.error(error);
+    }
+}
+
+main();
